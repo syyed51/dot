d276f1cfa92de74679466bb3d0391888a6013be8
Tehseen <syyed51@gmail.com>
2017-01-05 13:16:10 +0800

    add script to mount ntfs drives using default macOS drivers

---
+++ scripts/ntfs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# add 'fstab' file to load NTFS drives
+
+echo "This script will add readability options in /etc/fstab file."
+echo "You will have to remove the file manually, if no longer needed"
+echo "-------NTFS Reader---------"
+echo "___________________________"
+df -h
+echo "___________________________"
+echo -n "NTFS drive name from above >"
+read LABEL
+
+echo "LABEL=$LABEL none ntfs rw,auto,nobrowse" > /etc/fstab
+
+echo "done..."
